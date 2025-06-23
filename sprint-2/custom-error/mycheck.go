d58ba204f0458e8f0d0d8893ca0deb38c9b6dbf0
Alina <alinapalonskaa0@gmail.com>
2025-06-23 16:59:45 +0300

    mycheck tasks

---
+++ sprint-2/custom-error/mycheck.go
@@ -0,0 +1,53 @@
+//go:build !solution
+
+package mycheck
+
+import (
+	"errors"
+	"strings"
+	"unicode"
+)
+
+type myError []string
+
+func (err *myError) AddError(newErr string) {
+	*err = append(*err, newErr)
+}
+
+func (err myError) Error() error {
+	if len(err) == 0 {
+		return nil
+	}
+	return errors.New(strings.Join(err, ";"))
+}
+
+func MyCheck(input string) error {
+	var errs myError
+
+	var spaceCounter int
+	var foundNumbers, isLong bool
+
+	if len(input) >= 20 {
+		isLong = true
+	}
+
+	for _, val := range input {
+		if unicode.IsDigit(val) {
+			foundNumbers = true
+		} else if val == ' ' {
+			spaceCounter++
+		}
+	}
+
+	if foundNumbers {
+		errs.AddError("found numbers")
+	}
+	if isLong {
+		errs.AddError("line is too long")
+	}
+	if spaceCounter != 2 {
+		errs.AddError("no two spaces")
+	}
+
+	return errs.Error()
+}
